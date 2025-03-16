7780a3d504b559f8ca52dc608df00369871592ab
Vladyslav Hnatiuk <aders1234@gmail.com>
2025-03-16 17:54:43 +0100

    Fix in memory cache: file version is stored by key now, not by file path

--- finecode/extension_runner/impls/inmemory_cache.py
+++ finecode/extension_runner/impls/inmemory_cache.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, TypeAlias
 
 from finecode_extension_api.interfaces import icache, ifilemanager, ilogger
+
+CacheKeyType: TypeAlias = str
 
 
 class InMemoryCache(icache.ICache):
@@ -11,12 +13,12 @@ class InMemoryCache(icache.ICache):
         self.file_manager = file_manager
         self.logger = logger
 
-        self.cache_by_file: dict[Path, tuple[str, dict[str, Any]]] = {}
+        self.cache_by_file: dict[Path, dict[CacheKeyType, tuple[str, Any]]] = {}
 
         # TODO: clear file cache when file changes
 
     async def save_file_cache(
-        self, file_path: Path, file_version: str, key: str, value: Any
+        self, file_path: Path, file_version: str, key: CacheKeyType, value: Any
     ) -> None:
         current_file_version = await self.file_manager.get_file_version(file_path)
 
@@ -26,36 +28,31 @@ class InMemoryCache(icache.ICache):
 
         if file_path not in self.cache_by_file:
             # no cache for file, create
-            self.cache_by_file[file_path] = (current_file_version, {})
-        elif self.cache_by_file[file_path][0] != current_file_version:
-            # cached value is outdated, clean it
-            self.cache_by_file[file_path] = (current_file_version, {})
+            self.cache_by_file[file_path] = {}
 
-        self.cache_by_file[file_path][1][key] = value
+        self.cache_by_file[file_path][key] = (file_version, value)
 
-    async def get_file_cache(self, file_path: Path, key: str) -> Any:
+    async def get_file_cache(self, file_path: Path, key: CacheKeyType) -> Any:
         try:
             file_cache = self.cache_by_file[file_path]
         except KeyError:
             self.logger.debug(f"No cache for file {file_path}, cache miss")
             raise icache.CacheMissException()
 
+        if key not in file_cache:
+            self.logger.debug(
+                f"No cache with key {key} for file {file_path}, cache miss"
+            )
+            raise icache.CacheMissException()
+
         current_file_version = await self.file_manager.get_file_version(file_path)
-        cached_file_version = file_cache[0]
+        cached_file_version = file_cache[key][0]
         if cached_file_version != current_file_version:
             self.logger.debug(
                 f"Cached value for file {file_path} is outdated, cache miss"
             )
             raise icache.CacheMissException()
         else:
-            try:
-                cached_value = file_cache[1][key]
-            except KeyError:
-                self.logger.debug(
-                    f"Cached value for file {file_path} doesn't contain key"
-                    f" {key}, cache miss"
-                )
-                raise icache.CacheMissException()
-
             self.logger.debug(f"Use cached value for {file_path}, key {key}")
+            cached_value = file_cache[key][1]
             return cached_value
