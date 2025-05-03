edb9e5f21b755a9df0501513e6001a9cda4c462b
Vladyslav Hnatiuk <aders1234@gmail.com>
2025-05-03 14:45:12 +0200

    Use older syntax in iprocessexecutor to make it compatible with python 3.10+

--- finecode_extension_api/finecode_extension_api/interfaces/iprocessexecutor.py
+++ finecode_extension_api/finecode_extension_api/interfaces/iprocessexecutor.py
@@ -2,7 +2,11 @@ import typing
 from asyncio import BaseProtocol
 
 
+T = typing.TypeVar('T')
+P = typing.ParamSpec('P')
+
+
 class IProcessExecutor(BaseProtocol):
-    async def submit[T, **P](
+    async def submit(
         self, func: typing.Callable[P, T], *args: P.args, **kwargs: P.kwargs
     ): ...
