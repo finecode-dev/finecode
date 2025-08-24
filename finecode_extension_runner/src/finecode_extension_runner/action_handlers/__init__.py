b5de3a8778aaffef981dcd9cd7f2a45b39a16556
Vladyslav Hnatiuk <aders1234@gmail.com>
2025-08-24 18:45:04 +0200

    Remove unused action handlers

--- finecode_extension_runner/src/finecode_extension_runner/action_handlers/__init__.py
+++ finecode_extension_runner/src/finecode_extension_runner/action_handlers/__init__.py
@@ -1,9 +1,7 @@
 from .dump_config import DumpConfigHandler
 from .dump_config_save import DumpConfigSaveHandler
-from .prepare_envs_dump_configs import PrepareEnvsDumpConfigsHandler
 from .prepare_envs_install_deps import PrepareEnvsInstallDepsHandler
 from .prepare_envs_read_configs import PrepareEnvsReadConfigsHandler
-from .prepare_runners_dump_configs import PrepareRunnersDumpConfigsHandler
 from .prepare_runners_install_runner_and_presets import (
     PrepareRunnersInstallRunnerAndPresetsHandler,
 )
@@ -11,11 +9,8 @@ from .prepare_runners_read_configs import PrepareRunnersReadConfigsHandler
 
 __all__ = [
     "DumpConfigHandler",
-    "PrepareEnvsDumpConfigsHandler",
     "PrepareEnvsInstallDepsHandler",
     "PrepareEnvsReadConfigsHandler",
-    # not used
-    "PrepareRunnersDumpConfigsHandler",
     "PrepareRunnersInstallRunnerAndPresetsHandler",
     "PrepareRunnersReadConfigsHandler",
     "DumpConfigSaveHandler",
