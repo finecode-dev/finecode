cfc79a6886d8c1efa2956e3d144d747ea895010a
Vladyslav Hnatiuk <aders1234@gmail.com>
2026-01-27 08:30:38 +0100

    Extend isort config and make it compatible in fine_python_format with fine_python_lint

--- extensions/fine_python_isort/fine_python_isort/action.py
+++ extensions/fine_python_isort/fine_python_isort/action.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 import isort.api as isort_api
 import isort.settings as isort_settings
-
 from finecode_extension_api import code_action
 from finecode_extension_api.actions import format_files as format_files_action
 from finecode_extension_api.interfaces import icache, ilogger, iprocessexecutor
@@ -14,7 +13,14 @@ from finecode_extension_api.interfaces import icache, ilogger, iprocessexecutor
 
 @dataclasses.dataclass
 class IsortFormatFilesHandlerConfig(code_action.ActionHandlerConfig):
-    profile: str = ""
+    profile: str | None = None
+    line_length: int | None = None
+    multi_line_output: int | None = None
+    include_trailing_comma: bool | None = None
+    force_grid_wrap: int | None = None
+    use_parentheses: bool | None = None
+    ensure_newline_before_comments: bool | None = None
+    split_on_trailing_comma: bool | None = None
 
 
 class IsortFormatFilesHandler(
@@ -56,21 +62,37 @@ class IsortFormatFilesHandler(
                 changed=file_changed, code=new_file_content
             )
 
-        return format_files_action.FormatFilesRunResult(result_by_file_path=result_by_file_path)
+        return format_files_action.FormatFilesRunResult(
+            result_by_file_path=result_by_file_path
+        )
 
 
 def format_one(
     file_content: str, handler_config: IsortFormatFilesHandlerConfig
 ) -> tuple[str, bool]:
+    isort_config_overrides = {}
+    for param in [
+        "profile",
+        "line_length",
+        "multi_line_output",
+        "include_trailing_comma",
+        "force_grid_wrap",
+        "use_parentheses",
+        "ensure_newline_before_comments",
+        "line_length",
+        "split_on_trailing_comma",
+    ]:
+        handler_config_value = getattr(handler_config, param)
+        if handler_config_value is not None:
+            isort_config_overrides[param] = handler_config_value
+
     input_stream = StringIO(file_content)
     output_stream_context = isort_api._in_memory_output_stream_context()
     with output_stream_context as output_stream:
         changed = isort_api.sort_stream(
             input_stream=input_stream,
             output_stream=output_stream,
-            config=isort_settings.Config(
-                profile=handler_config.profile
-            ),  # TODO: config
+            config=isort_settings.Config(**isort_config_overrides),
             file_path=None,
             disregard_skip=True,
             extension=".py",
