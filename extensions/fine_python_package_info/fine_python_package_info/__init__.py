97f20ff4e9dbd31f47771a6dcd1f205d67afb412
Vladyslav Hnatiuk <aders1234@gmail.com>
2026-01-27 08:30:02 +0100

    Export all new handlers from fine_python_package_info

--- extensions/fine_python_package_info/fine_python_package_info/__init__.py
+++ extensions/fine_python_package_info/fine_python_package_info/__init__.py
@@ -1,9 +1,24 @@
-from .group_src_artifact_files_by_lang_python import GroupSrcArtifactFilesByLangPythonHandler
-from .list_src_artifact_files_by_lang_python import ListSrcArtifactFilesByLangPythonHandler
-from .py_package_layout_info_provider import PyPackageLayoutInfoProvider
+from .build_artifact_py_handler import BuildArtifactPyHandler
+from .get_src_artifact_registries_py_handler import \
+    GetSrcArtifactRegistriesPyHandler
 from .get_src_artifact_version_py_handler import GetSrcArtifactVersionPyHandler
-from .get_src_artifact_registries_py_handler import GetSrcArtifactRegistriesPyHandler
-from .publish_src_artifact_to_registry_py_handler import PublishSrcArtifactToRegistryPyHandler
-from .is_artifact_published_to_registry_py_handler import IsArtifactPublishedToRegistryPyHandler
+from .group_src_artifact_files_by_lang_python import \
+    GroupSrcArtifactFilesByLangPythonHandler
+from .is_artifact_published_to_registry_py_handler import \
+    IsArtifactPublishedToRegistryPyHandler
+from .list_src_artifact_files_by_lang_python import \
+    ListSrcArtifactFilesByLangPythonHandler
+from .publish_artifact_to_registry_py_handler import \
+    PublishArtifactToRegistryPyHandler
+from .py_package_layout_info_provider import PyPackageLayoutInfoProvider
 
-__all__ = ["GroupSrcArtifactFilesByLangPythonHandler","ListSrcArtifactFilesByLangPythonHandler", "PyPackageLayoutInfoProvider", "GetSrcArtifactVersionPyHandler", "GetSrcArtifactRegistriesPyHandler", "PublishSrcArtifactToRegistryPyHandler", "IsArtifactPublishedToRegistryPyHandler"]
+__all__ = [
+    "BuildArtifactPyHandler",
+    "GroupSrcArtifactFilesByLangPythonHandler",
+    "ListSrcArtifactFilesByLangPythonHandler",
+    "PyPackageLayoutInfoProvider",
+    "GetSrcArtifactVersionPyHandler",
+    "GetSrcArtifactRegistriesPyHandler",
+    "PublishArtifactToRegistryPyHandler",
+    "IsArtifactPublishedToRegistryPyHandler",
+]
