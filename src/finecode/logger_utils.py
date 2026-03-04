22c189b0f392c7464cde877a4efd99a116dca910
Vladyslav Hnatiuk <aders1234@gmail.com>
2026-03-04 10:04:24 +0100

    Create log directory for each process to make log files easier to find

--- src/finecode/logger_utils.py
+++ src/finecode/logger_utils.py
@@ -24,7 +24,7 @@ def init_logger(log_name: str, trace: bool, stdout: bool = False):
     )
     logs.set_log_level_for_group(group="finecode_jsonrpc.client", level=logs.LogLevel.INFO)
     logs.save_logs_to_file(
-        file_path=logs_dir_path / f"{log_name}.log",
+        file_path=logs_dir_path / log_name / f"{log_name}.log",
         log_level="TRACE" if trace else "INFO",
         stdout=stdout,
     )
